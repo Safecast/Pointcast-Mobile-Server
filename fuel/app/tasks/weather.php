e925e09163cd015a50426ae8612892f60e4e1cd5
OkadaMitsuo <leverages.mitsuo.okada@hipposlab.co.jp>
2015-11-24 22:43:15 +0900

    bugfix

--- fuel/app/tasks/weather.php
+++ fuel/app/tasks/weather.php
@@ -8,7 +8,14 @@ class Weather
 
     public function run()
     {
-       $this->getCurrentWeather(100);
+        $m_sensor_mains = \DB::select()
+                ->from('m_sensor_main')
+                ->where('enable', 1)
+                ->order_by('view_order', 'ASC')
+                ->execute()->as_array();
+        foreach ($m_sensor_mains as $key => $m_sensor_main) {
+            $this->getCurrentWeather($m_sensor_main['sensor1_device_id']);
+        }
     }
 
     public function getCurrentWeather($device_id)
@@ -37,17 +44,53 @@ var_dump($url);
 
         // decode json
         $current_weather = json_decode($response);
-var_dump($current_weather);
+        $l_weather_history = \Model_L_Weather_History::query()
+            ->where("original_id", $current_weather->id)
+            ->where("sensor1_device_id", $device_id)
+            ->get_one();
+        
+        if (empty($l_weather_history)) {
+            $l_weather_history = \Model_L_Weather_History::forge();
+        }
+
+        $l_weather_history->sensor1_device_id = $device_id;
+        $l_weather_history->coord_lon = $current_weather->coord->lon;
+        $l_weather_history->coord_lat = $current_weather->coord->lat;
+        $l_weather_history->weather_id = $current_weather->weather[0]->id;
+        $l_weather_history->weather_main = $current_weather->weather[0]->main;
+        $l_weather_history->weather_description = $current_weather->weather[0]->description;
+        $l_weather_history->icon = $current_weather->weather[0]->icon;
+        $l_weather_history->base = $current_weather->base;
+        $l_weather_history->main_temp = $current_weather->main->temp;
+        $l_weather_history->main_pressure = $current_weather->main->pressure;
+        $l_weather_history->main_humidity = $current_weather->main->humidity;
+        $l_weather_history->temp_min = $current_weather->main->temp_min;
+        $l_weather_history->temp_max = $current_weather->main->temp_max;
+        if (isset($current_weather->visibility)) {
+            $l_weather_history->visibility = $current_weather->visibility;
+        } else {
+            $l_weather_history->visibility = 0;
+        }
+        $l_weather_history->wind_speed = $current_weather->wind->speed;
+        $l_weather_history->wind_deg = $current_weather->wind->deg;
+        $l_weather_history->clouds_all = $current_weather->clouds->all;
+        $l_weather_history->dt = $current_weather->dt;
+        if (isset($current_weather->sys->type)) {
+            $l_weather_history->sys_type = $current_weather->sys->type;
+        } else {
+            $l_weather_history->sys_type = 0;
+        }
+        $l_weather_history->sys_id = $current_weather->id;
+        $l_weather_history->sys_message = $current_weather->sys->message;
+        $l_weather_history->sys_country = $current_weather->sys->country;
+        $l_weather_history->sys_sunrise = $current_weather->sys->sunrise;
+        $l_weather_history->sys_sunset = $current_weather->sys->sunset;
+        $l_weather_history->original_id = $current_weather->id;
+        $l_weather_history->name = $current_weather->name;
+        $l_weather_history->cod = $current_weather->cod;
+        $l_weather_history->save();
 
         return $current_weather;
-
-
-
-
-
-        $url = "http://api.openweathermap.org/data/2.5/weather?lcfirst(str)at=35&lon=139&appid=2de143494c0b295cca9337e1e96b00e0s";
-
-
     }
     
 }
