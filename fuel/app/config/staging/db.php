70b703e1f742f683c10b91c7c61c2db5b028e219
OkadaMitsuo <leverages.mitsuo.okada@hipposlab.co.jp>
2016-01-06 21:04:39 +0900

    staging db

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -1,14 +1,36 @@
 <?php
 /**
- * The staging database settings. These get merged with the global settings.
+ * The development database settings. These get merged with the global settings.
  */
 
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host=localhost;dbname=staging_pointcast;unix_socket=/tmp/mysql.sock',
+			'username'   => 'pointcast',
+			'password'   => 'pointcast',
 		),
 	),
+
+    'create_orm_model' => array(
+        'type'           => 'mysqli',
+        'connection'     => array(
+                'hostname'       => 'localhost',
+                'port'           => '3306',
+                'database'       => 'staging_pointcast',
+                'username'       => 'pointcast',
+                'password'       => 'pointcast',
+                'persistent'     => false,
+                'compress'       => false,
+                // 'socket' => '/var/run/mysqld/mysqld.sock',
+                'socket' => '/tmp/mysql.sock',
+                                
+        ),
+        'identifier'     => '`',
+        'table_prefix'   => '',
+        'charset'        => 'utf8',
+        'enable_cache'   => true,
+        'profiling'      => false,
+        'readonly'       => false,
+    ),
 );
