0961236872ceae65b3db953e9bb4b742919b7a42
OkadaMitsuo <leverages.mitsuo.okada@hipposlab.co.jp>
2016-01-06 21:20:04 +0900

    bugfix

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=staging_pointcast;unix_socket=/tmp/mysql.sock',
+			'dsn'        => 'mysql:host=localhost;dbname=pointcast_staging;unix_socket=/tmp/mysql.sock',
 			'username'   => 'pointcast',
 			'password'   => 'pointcast',
 		),
@@ -17,7 +17,7 @@ return array(
         'connection'     => array(
                 'hostname'       => 'localhost',
                 'port'           => '3306',
-                'database'       => 'staging_pointcast',
+                'database'       => 'pointcast_staging',
                 'username'       => 'pointcast',
                 'password'       => 'pointcast',
                 'persistent'     => false,
