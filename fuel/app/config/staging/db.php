4c103b19b1a3c8cbfc5d3940f45632cfe8c1016c
岡田光央 <mitsuo@okadamitsuo-no-Mac-mini.local>
2016-07-23 00:37:33 +0900

    update database setting

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -6,9 +6,9 @@
 return array(
     'default' => array(
         'connection'  => array(
-            'dsn'        => 'mysql:host=localhost;dbname=pointcast_staging;unix_socket=/tmp/mysql.sock',
-            'username'   => 'pointcast',
-            'password'   => 'pointcast',
+            'dsn'        => 'mysql:host=localhost;dbname=pointcast_production',
+            'username'   => 'root',
+            'password'   => '',
         ),
     ),
 
