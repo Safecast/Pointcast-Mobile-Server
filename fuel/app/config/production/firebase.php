800223a9bca13be999b009f427822a197e56d663
Mitsuo Okada <mitsuo-okada@united.jp>
2016-10-16 21:48:50 +0900

    update composer

--- fuel/app/config/production/firebase.php
+++ fuel/app/config/production/firebase.php
@@ -4,3 +4,6 @@ return array
 	'server_key' => 'AIzaSyCXkebrOdLMWB2tCi1IA9QCxgPJroOCqcc',
 );
 ?>
+
+
+curl --header "Authorization: key=AIzaSyCXkebrOdLMWB2tCi1IA9QCxgPJroOCqcc"      --header Content-Type:"application/json"      https://fcm.googleapis.com/fcm/send      -d "{\"to\": \"/topics/all\",\"priority\":\"high\",\"content_available\":true,\"notification\": {\"body\": \"お知ら せ\",\"badge\": \"1\"}}"
